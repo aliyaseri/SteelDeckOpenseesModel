1c49bff5375ac3997b49c0400eb0d8c49b519a93
AliYaseri <aliyaseripour@gmail.com>
2018-01-26 22:03:24 +0330

    init model

---
+++ element.tcl
@@ -0,0 +1,49 @@
+element 	nonlinearBeamColumn	1	1	2	10	360	1	7850
+element 	nonlinearBeamColumn	2	2	3	10	360	1	7850
+element 	nonlinearBeamColumn	3	3	4	10	360	1	7850
+element 	nonlinearBeamColumn	4	4	5	10	360	1	7850
+element 	nonlinearBeamColumn	5	5	6	10	360	1	7850
+element 	nonlinearBeamColumn	6	6	7	10	360	1	7850
+element 	nonlinearBeamColumn	7	7	8	10	360	1	7850
+element 	nonlinearBeamColumn	8	8	9	10	360	1	7850
+element 	nonlinearBeamColumn	9	9	10	10	360	1	7850
+element 	nonlinearBeamColumn	10	10	11	10	360	1	7850
+element 	nonlinearBeamColumn	11	11	12	10	360	1	7850
+element 	nonlinearBeamColumn	12	12	13	10	360	1	7850
+element 	nonlinearBeamColumn	13	13	14	10	360	1	7850
+element 	nonlinearBeamColumn	14	14	15	10	360	1	7850
+element 	nonlinearBeamColumn	15	15	16	10	360	1	7850
+
+element 	shellMITC4 	16	49	50	18	17	1
+element 	shellMITC4 	17	50	51	19	18	1
+element 	shellMITC4 	18	51	52	20	19	1
+element 	shellMITC4 	19	52	53	21	20	1
+element 	shellMITC4 	20	53	54	22	21	1
+element 	shellMITC4 	21	54	55	23	22	1
+element 	shellMITC4 	22	55	56	24	23	1
+element 	shellMITC4 	23	56	57	25	24	1
+element 	shellMITC4 	24	57	58	26	25	1
+element 	shellMITC4 	25	58	59	27	26	1
+element 	shellMITC4 	26	59	60	28	27	1
+element 	shellMITC4 	27	60	61	29	28	1
+element 	shellMITC4 	28	61	62	30	29	1
+element 	shellMITC4 	29	62	63	31	30	1
+element 	shellMITC4 	30	63	64	32	31	1
+
+element 	shellMITC4 	31	17	18	34	33	1
+element 	shellMITC4 	32	18	19	35	34	1
+element 	shellMITC4 	33	19	20	36	35	1
+element 	shellMITC4 	34	20	21	37	36	1
+element 	shellMITC4 	35	21	22	38	37	1
+element 	shellMITC4 	36	22	23	39	38	1
+element 	shellMITC4 	37	23	24	40	39	1
+element 	shellMITC4 	38	24	25	41	40	1
+element 	shellMITC4 	39	25	26	42	41	1
+element 	shellMITC4 	40	26	27	43	42	1
+element 	shellMITC4 	41	27	28	44	43	1
+element 	shellMITC4 	42	28	29	45	44	1
+element 	shellMITC4 	43	29	30	46	45	1
+element 	shellMITC4 	44	30	31	47	46	1
+element 	shellMITC4 	45	31	32	48	47	1
+
+
---
+++ model.tcl
@@ -0,0 +1,181 @@
+
+#3D Steel Deck Model
+#Written by ali yaseripour 
+#Unit N,m
+
+wipe
+model BasicBuilder -ndm 3 -ndf 6
+
+source node.tcl
+
+
+#define Rigidity 
+
+
+#rigidLink 	beam	master node 	slave node
+#rigidLink 	beam	1	17
+#rigidLink 	beam	2	18
+#rigidLink 	beam	3	19
+#rigidLink 	beam	4	20
+#rigidLink 	beam	5	21
+#rigidLink 	beam	6	22
+#rigidLink 	beam	7	23
+#rigidLink 	beam	8	24
+#rigidLink 	beam	9	25
+#rigidLink 	beam	10	26
+#rigidLink 	beam	11	27
+#rigidLink 	beam	12	28
+#rigidLink 	beam	13	29
+#rigidLink 	beam	14	30
+#rigidLink 	beam	15	31
+#rigidLink 	beam	16	32
+
+
+
+
+
+
+#
+
+#Define Constrain	
+#     ndTag ux    uy    uz    rx    ry    rz  1 fix 0free;															
+fix 	1	1	1	1	1	1	1 ;
+fix 	16	1	1	1	1	1	0 ; 
+
+uniaxialMaterial   Steel02        1 300e6   202.7e9  0.000001 18.5 0.925 0.15
+uniaxialMaterial   Steel02        2 415e6   202.7e9  0.000001 18.5 0.925 0.15
+
+
+#nDmaterial PlaneStressUserMaterial $matTag 40 7 $fc $ft $fcu $epsc0 $epscu $epstu $stc
+#                                                               fc         ft    fcu     epsc0     epscu  epstu  stc
+nDMaterial PlaneStressUserMaterial    3       40         7    41.0e6     2.08e6 -6.16e6  -0.002    -0.005 0.001 0.05
+ 
+#nDmaterial PlateFromPlaneStress $newmatTag $matTag $OutofPlaneModulus
+# nDMaterial PlateFromPlaneStress $matTag $PlaneStressMatTag $OutOfPlaneShearModulus #?????
+nDMaterial   PlateFromPlaneStress    4        3   1.283e10
+
+#nDmaterial PlateRebar $newmatTag $matTag $sita
+
+nDMaterial PlateRebar 5 2 90
+nDMaterial PlateRebar 6 2 0
+
+
+#section PlateFiber 1 3 0.156
+#long 0.00238
+#tra 0.000267
+#section LayeredShell $sectionTag $nLayers $matTag1 $thickness1...$matTagn $thicknessn
+section LayeredShell 1 3 4 0.156 5 0.00238 6 0.000267;
+
+
+set E 2.001E11;
+set G [expr $E/(2.0*(1.0+0.3))];
+
+section Fiber 360 -GJ [expr $G*6.21E-05] { ;
+patch	quad	1	10	3	-0.08285	0.1415	0.08285	0.1415	0.08285	0.1533	-0.08285	0.1533
+patch	quad	1	3	10	-0.00335	-0.1415	0.00335	-0.1415	0.00335	0.1415	-0.00335	0.1415
+patch	quad	1	10	3	-0.08285	-0.1533	0.08285	-0.1533	0.08285	-0.1415	-0.08285	-0.1415
+
+} ;
+
+geomTransf PDelta 1 0 1 0;
+#geomTransf Linear 2 0 0 -1;
+
+source element.tcl
+
+##element 	truss	eleTag	I 	j	A	matTag
+#element 	truss	46	1	17	0.1	2
+#element 	truss	47	2	18	0.1	2
+#element 	truss	48	3	19	0.1	2
+#element 	truss	49	4	20	0.1	2
+#element 	truss	50	5	21	0.1	2
+#element 	truss	51	6	22	0.1	2
+#element 	truss	52	7	23	0.1	2
+#element 	truss	53	8	24	0.1	2
+#element 	truss	54	9	25	0.1	2
+#element 	truss	55	10	26	0.1	2
+#element 	truss	56	11	27	0.1	2
+#element 	truss	57	12	28	0.1	2
+#element 	truss	58	13	29	0.1	2
+#element 	truss	59	14	30	0.1	2
+#element 	truss	60	15	31	0.1	2
+#element 	truss	61	16	32	0.1	2
+
+
+#element 	beam	eleTag	I 	j	A	E	G	J	Iz	Iy	transtag	mass
+element 	elasticBeamColumn	46	1	17	0.000314159	2.10E+11	80769230769	1.5708E-08	7.85398E-09	7.85398E-09	1	7850
+element 	elasticBeamColumn	47	2	18	0.000314159	2.10E+11	80769230769	1.5708E-08	7.85398E-09	7.85398E-09	1	7850
+element 	elasticBeamColumn	48	3	19	0.000314159	2.10E+11	80769230769	1.5708E-08	7.85398E-09	7.85398E-09	1	7850
+element 	elasticBeamColumn	49	4	20	0.000314159	2.10E+11	80769230769	1.5708E-08	7.85398E-09	7.85398E-09	1	7850
+element 	elasticBeamColumn	50	5	21	0.000314159	2.10E+11	80769230769	1.5708E-08	7.85398E-09	7.85398E-09	1	7850
+element 	elasticBeamColumn	51	6	22	0.000314159	2.10E+11	80769230769	1.5708E-08	7.85398E-09	7.85398E-09	1	7850
+element 	elasticBeamColumn	52	7	23	0.000314159	2.10E+11	80769230769	1.5708E-08	7.85398E-09	7.85398E-09	1	7850
+element 	elasticBeamColumn	53	8	24	0.000314159	2.10E+11	80769230769	1.5708E-08	7.85398E-09	7.85398E-09	1	7850
+element 	elasticBeamColumn	54	9	25	0.000314159	2.10E+11	80769230769	1.5708E-08	7.85398E-09	7.85398E-09	1	7850
+element 	elasticBeamColumn	55	10	26	0.000314159	2.10E+11	80769230769	1.5708E-08	7.85398E-09	7.85398E-09	1	7850
+element 	elasticBeamColumn	56	11	27	0.000314159	2.10E+11	80769230769	1.5708E-08	7.85398E-09	7.85398E-09	1	7850
+element 	elasticBeamColumn	57	12	28	0.000314159	2.10E+11	80769230769	1.5708E-08	7.85398E-09	7.85398E-09	1	7850
+element 	elasticBeamColumn	58	13	29	0.000314159	2.10E+11	80769230769	1.5708E-08	7.85398E-09	7.85398E-09	1	7850
+element 	elasticBeamColumn	59	14	30	0.000314159	2.10E+11	80769230769	1.5708E-08	7.85398E-09	7.85398E-09	1	7850
+element 	elasticBeamColumn	60	15	31	0.000314159	2.10E+11	80769230769	1.5708E-08	7.85398E-09	7.85398E-09	1	7850
+element 	elasticBeamColumn	61	16	32	0.000314159	2.10E+11	80769230769	1.5708E-08	7.85398E-09	7.85398E-09	1	7850
+
+
+#element 	elasticBeamColumn	46	1	17	0.125663706	2.10E+11	80769230769	0.002513274	0.001256637	0.001256637	1	7850
+#element 	elasticBeamColumn	47	2	18	0.125663706	2.10E+11	80769230769	0.002513274	0.001256637	0.001256637	1	7850
+#element 	elasticBeamColumn	48	3	19	0.125663706	2.10E+11	80769230769	0.002513274	0.001256637	0.001256637	1	7850
+#element 	elasticBeamColumn	49	4	20	0.125663706	2.10E+11	80769230769	0.002513274	0.001256637	0.001256637	1	7850
+#element 	elasticBeamColumn	50	5	21	0.125663706	2.10E+11	80769230769	0.002513274	0.001256637	0.001256637	1	7850
+#element 	elasticBeamColumn	51	6	22	0.125663706	2.10E+11	80769230769	0.002513274	0.001256637	0.001256637	1	7850
+#element 	elasticBeamColumn	52	7	23	0.125663706	2.10E+11	80769230769	0.002513274	0.001256637	0.001256637	1	7850
+#element 	elasticBeamColumn	53	8	24	0.125663706	2.10E+11	80769230769	0.002513274	0.001256637	0.001256637	1	7850
+#element 	elasticBeamColumn	54	9	25	0.125663706	2.10E+11	80769230769	0.002513274	0.001256637	0.001256637	1	7850
+#element 	elasticBeamColumn	55	10	26	0.125663706	2.10E+11	80769230769	0.002513274	0.001256637	0.001256637	1	7850
+#element 	elasticBeamColumn	56	11	27	0.125663706	2.10E+11	80769230769	0.002513274	0.001256637	0.001256637	1	7850
+#element 	elasticBeamColumn	57	12	28	0.125663706	2.10E+11	80769230769	0.002513274	0.001256637	0.001256637	1	7850
+#element 	elasticBeamColumn	58	13	29	0.125663706	2.10E+11	80769230769	0.002513274	0.001256637	0.001256637	1	7850
+#element 	elasticBeamColumn	59	14	30	0.125663706	2.10E+11	80769230769	0.002513274	0.001256637	0.001256637	1	7850
+#element 	elasticBeamColumn	60	15	31	0.125663706	2.10E+11	80769230769	0.002513274	0.001256637	0.001256637	1	7850
+#element 	elasticBeamColumn	61	16	32	0.125663706	2.10E+11	80769230769	0.002513274	0.001256637	0.001256637	1	7850
+
+
+
+recorder Node -file AnewD.out -time -node  7 -dof 3 disp
+recorder Node -file AnewR1.out -time -node  1 -dof 3 reaction
+recorder Node -file AnewR21.out -time -node  16 -dof 3 reaction
+
+#load
+pattern Plain 1 Linear { ;
+#eleLoad -ele $eleTag1 <$eleTag2 ....> -type -beamUniform $Wy <$Wx>
+load 7 0.0 0.0 10.0 0.0 0.0 0.0  
+#load 33 0.0 0.0 10.0 0.0 0.0 0.0 
+};
+
+
+#constraints Transformation
+#numberer RCM
+#system UmfPack 
+#test EnergyIncr 1.0e-5 25 0
+#algorithm ModifiedNewton
+
+constraints Transformation;
+numberer RCM;
+##system BandGeneral;
+system UmfPack 
+##test NormDispIncr 1.0e-3 200 ;
+test EnergyIncr 1.0e-5 200 0;
+##algorithm BFGS -count 200
+##algorithm NewtonLineSearch
+algorithm KrylovNewton;
+##algorithm ModifiedNewton
+##
+integrator DisplacementControl 7 3 -0.0001
+##integrator LoadControl 50;	
+analysis Static;
+
+#
+#source Solver.tcl
+#integrator DisplacementControl 10 3 0.001;
+analyze 10000;
+loadConst -time 0.0;
+puts "    pushover is built!"
+exit;
