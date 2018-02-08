5cf5832aa4335cef434e078133a111b5efbd232e
AliYaseri <aliyaseripour@gmail.com>
2018-02-08 16:57:17 +0330

    creat impact3D element

--- model.tcl
+++ model.tcl
@@ -9,83 +9,114 @@ model BasicBuilder -ndm 3 -ndf 6
 source node.tcl
 
 
-#define Rigidity 
-
-
-#rigidLink 	beam	master node 	slave node
-#rigidLink 	beam	1	17
-#rigidLink 	beam	2	18
-#rigidLink 	beam	3	19
-#rigidLink 	beam	4	20
-#rigidLink 	beam	5	21
-#rigidLink 	beam	6	22
-#rigidLink 	beam	7	23
-#rigidLink 	beam	8	24
-#rigidLink 	beam	9	25
-#rigidLink 	beam	10	26
-#rigidLink 	beam	11	27
-#rigidLink 	beam	12	28
-#rigidLink 	beam	13	29
-#rigidLink 	beam	14	30
-#rigidLink 	beam	15	31
-#rigidLink 	beam	16	32
-
 
 model BasicBuilder -ndm 3 -ndf 3
 
-node 	65	0	0	0
-node 	66	0.143	0	0
-node 	67	0.4287	0	0
-node 	68	0.7144	0	0
-node 	69	1.0001	0	0
-node 	70	1.2858	0	0
-node 	71	1.5715	0	0
-node 	72	1.8572	0	0
-node 	73	2.1429	0	0
-node 	74	2.4286	0	0
-node 	75	2.7143	0	0
-node 	76	3	0	0
-node 	77	3.2857	0	0
-node 	78	3.5714	0	0
-node 	79	3.8571	0	0
-node 	80	4.0001	0	0
+node 	65	0	0	0.049
+node 	66	0.143	0	0.049
+node 	67	0.4287	0	0.049
+node 	68	0.7144	0	0.049
+node 	69	1.0001	0	0.049
+node 	70	1.2858	0	0.049
+node 	71	1.5715	0	0.049
+node 	72	1.8572	0	0.049
+node 	73	2.1429	0	0.049
+node 	74	2.4286	0	0.049
+node 	75	2.7143	0	0.049
+node 	76	3	0	0.049
+node 	77	3.2857	0	0.049
+node 	78	3.5714	0	0.049
+node 	79	3.8571	0	0.049
+node 	80	4.0001	0	0.049
 
-node 	81	0	0	0.1
-node 	82	0.143	0	0.1
-node 	83	0.4287	0	0.1
-node 	84	0.7144	0	0.1
-node 	85	1.0001	0	0.1
-node 	86	1.2858	0	0.1
-node 	87	1.5715	0	0.1
-node 	88	1.8572	0	0.1
-node 	89	2.1429	0	0.1
-node 	90	2.4286	0	0.1
-node 	91	2.7143	0	0.1
-node 	92	3	0	0.1
-node 	93	3.2857	0	0.1
-node 	94	3.5714	0	0.1
-node 	95	3.8571	0	0.1
-node 	96	4.0001	0	0.1
+
+node 	81	0	0.1415	0.049
+node 	82	0.143	0.1415	0.049
+node 	83	0.4287	0.1415	0.049
+node 	84	0.7144	0.1415	0.049
+node 	85	1.0001	0.1415	0.049
+node 	86	1.2858	0.1415	0.049
+node 	87	1.5715	0.1415	0.049
+node 	88	1.8572	0.1415	0.049
+node 	89	2.1429	0.1415	0.049
+node 	90	2.4286	0.1415	0.049
+node 	91	2.7143	0.1415	0.049
+node 	92	3	0.1415	0.049
+node 	93	3.2857	0.1415	0.049
+node 	94	3.5714	0.1415	0.049
+node 	95	3.8571	0.1415	0.049
+node 	96	4.0001	0.1415	0.049
+
+node 	97	0	-0.1415	0.049
+node 	98	0.143	-0.1415	0.049
+node 	99	0.4287	-0.1415	0.049
+node 	100	0.7144	-0.1415	0.049
+node 	101	1.0001	-0.1415	0.049
+node 	102	1.2858	-0.1415	0.049
+node 	103	1.5715	-0.1415	0.049
+node 	104	1.8572	-0.1415	0.049
+node 	105	2.1429	-0.1415	0.049
+node 	106	2.4286	-0.1415	0.049
+node 	107	2.7143	-0.1415	0.049
+node 	108	3	-0.1415	0.049
+node 	109	3.2857	-0.1415	0.049
+node 	110	3.5714	-0.1415	0.049
+node 	111	3.8571	-0.1415	0.049
+node 	112	4.0001	-0.1415	0.049
+
+node 	113	0	0	0.05
+node 	114	0.143	0	0.05
+node 	115	0.4287	0	0.05
+node 	116	0.7144	0	0.05
+node 	117	1.0001	0	0.05
+node 	118	1.2858	0	0.05
+node 	119	1.5715	0	0.05
+node 	120	1.8572	0	0.05
+node 	121	2.1429	0	0.05
+node 	122	2.4286	0	0.05
+node 	123	2.7143	0	0.05
+node 	124	3	0	0.05
+node 	125	3.2857	0	0.05
+node 	126	3.5714	0	0.05
+node 	127	3.8571	0	0.05
+node 	128	4.0001	0	0.05
+
+node 	129	0	0.3325	0.05
+node 	130	0.143	0.3325	0.05
+node 	131	0.4287	0.3325	0.05
+node 	132	0.7144	0.3325	0.05
+node 	133	1.0001	0.3325	0.05
+node 	134	1.2858	0.3325	0.05
+node 	135	1.5715	0.3325	0.05
+node 	136	1.8572	0.3325	0.05
+node 	137	2.1429	0.3325	0.05
+node 	138	2.4286	0.3325	0.05
+node 	139	2.7143	0.3325	0.05
+node 	140	3	0.3325	0.05
+node 	141	3.2857	0.3325	0.05
+node 	142	3.5714	0.3325	0.05
+node 	143	3.8571	0.3325	0.05
+node 	144	4.0001	0.3325	0.05
+
+node 	145	0	-0.3325	0.05
+node 	146	0.143	-0.3325	0.05
+node 	147	0.4287	-0.3325	0.05
+node 	148	0.7144	-0.3325	0.05
+node 	149	1.0001	-0.3325	0.05
+node 	150	1.2858	-0.3325	0.05
+node 	151	1.5715	-0.3325	0.05
+node 	152	1.8572	-0.3325	0.05
+node 	153	2.1429	-0.3325	0.05
+node 	154	2.4286	-0.3325	0.05
+node 	155	2.7143	-0.3325	0.05
+node 	156	3	-0.3325	0.05
+node 	157	3.2857	-0.3325	0.05
+node 	158	3.5714	-0.3325	0.05
+node 	159	3.8571	-0.3325	0.05
+node 	160	4.0001	-0.3325	0.05
 
 
 model BasicBuilder -ndm 3 -ndf 6
-equalDOF 	17	81	1	2	3
-equalDOF 	18	82	1	2	3
-equalDOF 	19	83	1	2	3
-equalDOF 	20	84	1	2	3
-equalDOF 	21	85	1	2	3
-equalDOF 	22	86	1	2	3
-equalDOF 	23	87	1	2	3
-equalDOF 	24	88	1	2	3
-equalDOF 	25	89	1	2	3
-equalDOF 	26	90	1	2	3
-equalDOF 	27	91	1	2	3
-equalDOF 	28	92	1	2	3
-equalDOF 	29	93	1	2	3
-equalDOF 	30	94	1	2	3
-equalDOF 	31	95	1	2	3
-equalDOF 	32	96	1	2	3
-
 equalDOF 	1	65	1	2	3
 equalDOF 	2	66	1	2	3
 equalDOF 	3	67	1	2	3
@@ -103,6 +134,90 @@ equalDOF 	14	78	1	2	3
 equalDOF 	15	79	1	2	3
 equalDOF 	16	80	1	2	3
 
+equalDOF 	1	81	1	2	3
+equalDOF 	2	82	1	2	3
+equalDOF 	3	83	1	2	3
+equalDOF 	4	84	1	2	3
+equalDOF 	5	85	1	2	3
+equalDOF 	6	86	1	2	3
+equalDOF 	7	87	1	2	3
+equalDOF 	8	88	1	2	3
+equalDOF 	9	89	1	2	3
+equalDOF 	10	90	1	2	3
+equalDOF 	11	91	1	2	3
+equalDOF 	12	92	1	2	3
+equalDOF 	13	93	1	2	3
+equalDOF 	14	94	1	2	3
+equalDOF 	15	95	1	2	3
+equalDOF 	16	96	1	2	3
+
+equalDOF 	1	97	1	2	3
+equalDOF 	2	98	1	2	3
+equalDOF 	3	99	1	2	3
+equalDOF 	4	100	1	2	3
+equalDOF 	5	101	1	2	3
+equalDOF 	6	102	1	2	3
+equalDOF 	7	103	1	2	3
+equalDOF 	8	104	1	2	3
+equalDOF 	9	105	1	2	3
+equalDOF 	10	106	1	2	3
+equalDOF 	11	107	1	2	3
+equalDOF 	12	108	1	2	3
+equalDOF 	13	109	1	2	3
+equalDOF 	14	110	1	2	3
+equalDOF 	15	111	1	2	3
+equalDOF 	16	112	1	2	3
+
+equalDOF 	17	113	1	2	3
+equalDOF 	18	114	1	2	3
+equalDOF 	19	115	1	2	3
+equalDOF 	20	116	1	2	3
+equalDOF 	21	117	1	2	3
+equalDOF 	22	118	1	2	3
+equalDOF 	23	119	1	2	3
+equalDOF 	24	120	1	2	3
+equalDOF 	25	121	1	2	3
+equalDOF 	26	122	1	2	3
+equalDOF 	27	123	1	2	3
+equalDOF 	28	124	1	2	3
+equalDOF 	29	125	1	2	3
+equalDOF 	30	126	1	2	3
+equalDOF 	31	127	1	2	3
+equalDOF 	32	128	1	2	3
+
+equalDOF 	33	129	1	2	3
+equalDOF 	34	130	1	2	3
+equalDOF 	35	131	1	2	3
+equalDOF 	36	132	1	2	3
+equalDOF 	37	133	1	2	3
+equalDOF 	38	134	1	2	3
+equalDOF 	39	135	1	2	3
+equalDOF 	40	136	1	2	3
+equalDOF 	41	137	1	2	3
+equalDOF 	42	138	1	2	3
+equalDOF 	43	139	1	2	3
+equalDOF 	44	140	1	2	3
+equalDOF 	45	141	1	2	3
+equalDOF 	46	142	1	2	3
+equalDOF 	47	143	1	2	3
+equalDOF 	48	144	1	2	3
+
+equalDOF 	49	145	1	2	3
+equalDOF 	50	146	1	2	3
+equalDOF 	51	147	1	2	3
+equalDOF 	52	148	1	2	3
+equalDOF 	53	149	1	2	3
+equalDOF 	54	150	1	2	3
+equalDOF 	55	151	1	2	3
+equalDOF 	56	152	1	2	3
+equalDOF 	57	153	1	2	3
+equalDOF 	58	154	1	2	3
+equalDOF 	59	155	1	2	3
+equalDOF 	60	156	1	2	3
+equalDOF 	61	157	1	2	3
+equalDOF 	62	158	1	2	3
+equalDOF 	63	159	1	2	3
+equalDOF 	64	160	1	2	3
 
 
 #
@@ -158,23 +273,6 @@ geomTransf PDelta 1 0 1 0;
 
 source element.tcl
 
-##element 	truss	eleTag	I 	j	A	matTag
-#element 	truss	46	1	17	0.1	2
-#element 	truss	47	2	18	0.1	2
-#element 	truss	48	3	19	0.1	2
-#element 	truss	49	4	20	0.1	2
-#element 	truss	50	5	21	0.1	2
-#element 	truss	51	6	22	0.1	2
-#element 	truss	52	7	23	0.1	2
-#element 	truss	53	8	24	0.1	2
-#element 	truss	54	9	25	0.1	2
-#element 	truss	55	10	26	0.1	2
-#element 	truss	56	11	27	0.1	2
-#element 	truss	57	12	28	0.1	2
-#element 	truss	58	13	29	0.1	2
-#element 	truss	59	14	30	0.1	2
-#element 	truss	60	15	31	0.1	2
-#element 	truss	61	16	32	0.1	2
 
 #element 	beam	eleTag	I 	j	A	E	G	J	Iz	Iy	transtag	mass
 element 	elasticBeamColumn	46	1	17	0.000314159	2.10E+11	80769230769	1.5708E-08	7.85398E-09	7.85398E-09	1	7850
@@ -195,98 +293,63 @@ element 	elasticBeamColumn	60	15	31	0.000314159	2.10E+11	80769230769	1.5708E-08	
 element 	elasticBeamColumn	61	16	32	0.000314159	2.10E+11	80769230769	1.5708E-08	7.85398E-09	7.85398E-09	1	7850
 
 
-#element 	elasticBeamColumn	46	1	17	0.125663706	2.10E+11	80769230769	0.002513274	0.001256637	0.001256637	1	7850
-#element 	elasticBeamColumn	47	2	18	0.125663706	2.10E+11	80769230769	0.002513274	0.001256637	0.001256637	1	7850
-#element 	elasticBeamColumn	48	3	19	0.125663706	2.10E+11	80769230769	0.002513274	0.001256637	0.001256637	1	7850
-#element 	elasticBeamColumn	49	4	20	0.125663706	2.10E+11	80769230769	0.002513274	0.001256637	0.001256637	1	7850
-#element 	elasticBeamColumn	50	5	21	0.125663706	2.10E+11	80769230769	0.002513274	0.001256637	0.001256637	1	7850
-#element 	elasticBeamColumn	51	6	22	0.125663706	2.10E+11	80769230769	0.002513274	0.001256637	0.001256637	1	7850
-#element 	elasticBeamColumn	52	7	23	0.125663706	2.10E+11	80769230769	0.002513274	0.001256637	0.001256637	1	7850
-#element 	elasticBeamColumn	53	8	24	0.125663706	2.10E+11	80769230769	0.002513274	0.001256637	0.001256637	1	7850
-#element 	elasticBeamColumn	54	9	25	0.125663706	2.10E+11	80769230769	0.002513274	0.001256637	0.001256637	1	7850
-#element 	elasticBeamColumn	55	10	26	0.125663706	2.10E+11	80769230769	0.002513274	0.001256637	0.001256637	1	7850
-#element 	elasticBeamColumn	56	11	27	0.125663706	2.10E+11	80769230769	0.002513274	0.001256637	0.001256637	1	7850
-#element 	elasticBeamColumn	57	12	28	0.125663706	2.10E+11	80769230769	0.002513274	0.001256637	0.001256637	1	7850
-#element 	elasticBeamColumn	58	13	29	0.125663706	2.10E+11	80769230769	0.002513274	0.001256637	0.001256637	1	7850
-#element 	elasticBeamColumn	59	14	30	0.125663706	2.10E+11	80769230769	0.002513274	0.001256637	0.001256637	1	7850
-#element 	elasticBeamColumn	60	15	31	0.125663706	2.10E+11	80769230769	0.002513274	0.001256637	0.001256637	1	7850
-#element 	elasticBeamColumn	61	16	32	0.125663706	2.10E+11	80769230769	0.002513274	0.001256637	0.001256637	1	7850
 
-#element flatSliderBearing $eleTag $iNode $jNode $frnMdlTag $kInit -P $matTag -T $matTag -My $matTag -Mz $matTag <-orient <$x1 $x2 $x3> $y1 $y2 $y3> <-shearDist $sDratio> <-doRayleigh> <-mass $m> <-iter $maxIter $tol>
-#
-#element 	flatSliderBearing	62	65	81	1	1.00E+10	-P	50	-T	50	-My	50	-Mz	50
-#element 	flatSliderBearing	63	66	82	1	1.00E+10	-P	50	-T	50	-My	50	-Mz	50
-#element 	flatSliderBearing	64	67	83	1	1.00E+10	-P	50	-T	50	-My	50	-Mz	50
-#element 	flatSliderBearing	65	68	84	1	1.00E+10	-P	50	-T	50	-My	50	-Mz	50
-#element 	flatSliderBearing	66	69	85	1	1.00E+10	-P	50	-T	50	-My	50	-Mz	50
-#element 	flatSliderBearing	67	70	86	1	1.00E+10	-P	50	-T	50	-My	50	-Mz	50
-#element 	flatSliderBearing	68	71	87	1	1.00E+10	-P	50	-T	50	-My	50	-Mz	50
-#element 	flatSliderBearing	69	72	88	1	1.00E+10	-P	50	-T	50	-My	50	-Mz	50
-#element 	flatSliderBearing	70	73	89	1	1.00E+10	-P	50	-T	50	-My	50	-Mz	50
-#element 	flatSliderBearing	71	74	90	1	1.00E+10	-P	50	-T	50	-My	50	-Mz	50
-#element 	flatSliderBearing	72	75	91	1	1.00E+10	-P	50	-T	50	-My	50	-Mz	50
-#element 	flatSliderBearing	73	76	92	1	1.00E+10	-P	50	-T	50	-My	50	-Mz	50
-#element 	flatSliderBearing	74	77	93	1	1.00E+10	-P	50	-T	50	-My	50	-Mz	50
-#element 	flatSliderBearing	75	78	94	1	1.00E+10	-P	50	-T	50	-My	50	-Mz	50
-#element 	flatSliderBearing	76	79	95	1	1.00E+10	-P	50	-T	50	-My	50	-Mz	50
-#element 	flatSliderBearing	77	80	96	1	1.00E+10	-P	50	-T	50	-My	50	-Mz	50
+# element zeroLengthImpact3D $tag $slaveNode $masterNode $direction $initGap $frictionRatio $Kt $Kn $Kn2 $Delta_y $cohesion
+element 	zeroLengthImpact3D	62	65	113	3	0	0.4	1.00E+10	1.00E+10	1.00E+05	0.00E+00	0.00E+00
+element 	zeroLengthImpact3D	63	66	114	3	0	0.4	1.00E+10	1.00E+10	1.00E+05	0.00E+00	0.00E+00
+element 	zeroLengthImpact3D	64	67	115	3	0	0.4	1.00E+10	1.00E+10	1.00E+05	0.00E+00	0.00E+00
+element 	zeroLengthImpact3D	65	68	116	3	0	0.4	1.00E+10	1.00E+10	1.00E+05	0.00E+00	0.00E+00
+element 	zeroLengthImpact3D	66	69	117	3	0	0.4	1.00E+10	1.00E+10	1.00E+05	0.00E+00	0.00E+00
+element 	zeroLengthImpact3D	67	70	118	3	0	0.4	1.00E+10	1.00E+10	1.00E+05	0.00E+00	0.00E+00
+element 	zeroLengthImpact3D	68	71	119	3	0	0.4	1.00E+10	1.00E+10	1.00E+05	0.00E+00	0.00E+00
+element 	zeroLengthImpact3D	69	72	120	3	0	0.4	1.00E+10	1.00E+10	1.00E+05	0.00E+00	0.00E+00
+element 	zeroLengthImpact3D	70	73	121	3	0	0.4	1.00E+10	1.00E+10	1.00E+05	0.00E+00	0.00E+00
+element 	zeroLengthImpact3D	71	74	122	3	0	0.4	1.00E+10	1.00E+10	1.00E+05	0.00E+00	0.00E+00
+element 	zeroLengthImpact3D	72	75	123	3	0	0.4	1.00E+10	1.00E+10	1.00E+05	0.00E+00	0.00E+00
+element 	zeroLengthImpact3D	73	76	124	3	0	0.4	1.00E+10	1.00E+10	1.00E+05	0.00E+00	0.00E+00
+element 	zeroLengthImpact3D	74	77	125	3	0	0.4	1.00E+10	1.00E+10	1.00E+05	0.00E+00	0.00E+00
+element 	zeroLengthImpact3D	75	78	126	3	0	0.4	1.00E+10	1.00E+10	1.00E+05	0.00E+00	0.00E+00
+element 	zeroLengthImpact3D	76	79	127	3	0	0.4	1.00E+10	1.00E+10	1.00E+05	0.00E+00	0.00E+00
+element 	zeroLengthImpact3D	77	80	128	3	0	0.4	1.00E+10	1.00E+10	1.00E+05	0.00E+00	0.00E+00
+
+element 	zeroLengthImpact3D	78	81	129	3	0	0.4	1.00E+10	1.00E+10	1.00E+05	0.00E+00	0.00E+00
+element 	zeroLengthImpact3D	79	82	130	3	0	0.4	1.00E+10	1.00E+10	1.00E+05	0.00E+00	0.00E+00
+element 	zeroLengthImpact3D	80	83	131	3	0	0.4	1.00E+10	1.00E+10	1.00E+05	0.00E+00	0.00E+00
+element 	zeroLengthImpact3D	81	84	132	3	0	0.4	1.00E+10	1.00E+10	1.00E+05	0.00E+00	0.00E+00
+element 	zeroLengthImpact3D	82	85	133	3	0	0.4	1.00E+10	1.00E+10	1.00E+05	0.00E+00	0.00E+00
+element 	zeroLengthImpact3D	83	86	134	3	0	0.4	1.00E+10	1.00E+10	1.00E+05	0.00E+00	0.00E+00
+element 	zeroLengthImpact3D	84	87	135	3	0	0.4	1.00E+10	1.00E+10	1.00E+05	0.00E+00	0.00E+00
+element 	zeroLengthImpact3D	85	88	136	3	0	0.4	1.00E+10	1.00E+10	1.00E+05	0.00E+00	0.00E+00
+element 	zeroLengthImpact3D	86	89	137	3	0	0.4	1.00E+10	1.00E+10	1.00E+05	0.00E+00	0.00E+00
+element 	zeroLengthImpact3D	87	90	138	3	0	0.4	1.00E+10	1.00E+10	1.00E+05	0.00E+00	0.00E+00
+element 	zeroLengthImpact3D	88	91	139	3	0	0.4	1.00E+10	1.00E+10	1.00E+05	0.00E+00	0.00E+00
+element 	zeroLengthImpact3D	89	92	140	3	0	0.4	1.00E+10	1.00E+10	1.00E+05	0.00E+00	0.00E+00
+element 	zeroLengthImpact3D	90	93	141	3	0	0.4	1.00E+10	1.00E+10	1.00E+05	0.00E+00	0.00E+00
+element 	zeroLengthImpact3D	91	94	142	3	0	0.4	1.00E+10	1.00E+10	1.00E+05	0.00E+00	0.00E+00
+element 	zeroLengthImpact3D	92	95	143	3	0	0.4	1.00E+10	1.00E+10	1.00E+05	0.00E+00	0.00E+00
+element 	zeroLengthImpact3D	93	96	144	3	0	0.4	1.00E+10	1.00E+10	1.00E+05	0.00E+00	0.00E+00
+
+element 	zeroLengthImpact3D	78	97	145	3	0	0.4	1.00E+10	1.00E+10	1.00E+05	0.00E+00	0.00E+00
+element 	zeroLengthImpact3D	79	98	146	3	0	0.4	1.00E+10	1.00E+10	1.00E+05	0.00E+00	0.00E+00
+element 	zeroLengthImpact3D	80	99	147	3	0	0.4	1.00E+10	1.00E+10	1.00E+05	0.00E+00	0.00E+00
+element 	zeroLengthImpact3D	81	100	148	3	0	0.4	1.00E+10	1.00E+10	1.00E+05	0.00E+00	0.00E+00
+element 	zeroLengthImpact3D	82	101	149	3	0	0.4	1.00E+10	1.00E+10	1.00E+05	0.00E+00	0.00E+00
+element 	zeroLengthImpact3D	83	102	150	3	0	0.4	1.00E+10	1.00E+10	1.00E+05	0.00E+00	0.00E+00
+element 	zeroLengthImpact3D	84	103	151	3	0	0.4	1.00E+10	1.00E+10	1.00E+05	0.00E+00	0.00E+00
+element 	zeroLengthImpact3D	85	104	152	3	0	0.4	1.00E+10	1.00E+10	1.00E+05	0.00E+00	0.00E+00
+element 	zeroLengthImpact3D	86	105	153	3	0	0.4	1.00E+10	1.00E+10	1.00E+05	0.00E+00	0.00E+00
+element 	zeroLengthImpact3D	87	106	154	3	0	0.4	1.00E+10	1.00E+10	1.00E+05	0.00E+00	0.00E+00
+element 	zeroLengthImpact3D	88	107	155	3	0	0.4	1.00E+10	1.00E+10	1.00E+05	0.00E+00	0.00E+00
+element 	zeroLengthImpact3D	89	108	156	3	0	0.4	1.00E+10	1.00E+10	1.00E+05	0.00E+00	0.00E+00
+element 	zeroLengthImpact3D	90	109	157	3	0	0.4	1.00E+10	1.00E+10	1.00E+05	0.00E+00	0.00E+00
+element 	zeroLengthImpact3D	91	110	158	3	0	0.4	1.00E+10	1.00E+10	1.00E+05	0.00E+00	0.00E+00
+element 	zeroLengthImpact3D	92	111	159	3	0	0.4	1.00E+10	1.00E+10	1.00E+05	0.00E+00	0.00E+00
+element 	zeroLengthImpact3D	93	112	160	3	0	0.4	1.00E+10	1.00E+10	1.00E+05	0.00E+00	0.00E+00
 
 
-element 	flatSliderBearing	62	1	17	1	1.00E+10	-P	50	-T	50	-My	50	-Mz	50
-element 	flatSliderBearing	63	2	18	1	1.00E+10	-P	50	-T	50	-My	50	-Mz	50
-element 	flatSliderBearing	64	3	19	1	1.00E+10	-P	50	-T	50	-My	50	-Mz	50
-element 	flatSliderBearing	65	4	20	1	1.00E+10	-P	50	-T	50	-My	50	-Mz	50
-element 	flatSliderBearing	66	5	21	1	1.00E+10	-P	50	-T	50	-My	50	-Mz	50
-element 	flatSliderBearing	67	6	22	1	1.00E+10	-P	50	-T	50	-My	50	-Mz	50
-element 	flatSliderBearing	68	7	23	1	1.00E+10	-P	50	-T	50	-My	50	-Mz	50
-element 	flatSliderBearing	69	8	24	1	1.00E+10	-P	50	-T	50	-My	50	-Mz	50
-element 	flatSliderBearing	70	9	25	1	1.00E+10	-P	50	-T	50	-My	50	-Mz	50
-element 	flatSliderBearing	71	10	26	1	1.00E+10	-P	50	-T	50	-My	50	-Mz	50
-element 	flatSliderBearing	72	11	27	1	1.00E+10	-P	50	-T	50	-My	50	-Mz	50
-element 	flatSliderBearing	73	12	28	1	1.00E+10	-P	50	-T	50	-My	50	-Mz	50
-element 	flatSliderBearing	74	13	29	1	1.00E+10	-P	50	-T	50	-My	50	-Mz	50
-element 	flatSliderBearing	75	14	30	1	1.00E+10	-P	50	-T	50	-My	50	-Mz	50
-element 	flatSliderBearing	76	15	31	1	1.00E+10	-P	50	-T	50	-My	50	-Mz	50
-element 	flatSliderBearing	77	16	32	1	1.00E+10	-P	50	-T	50	-My	50	-Mz	50
-
-
-element 	flatSliderBearing	78	1	33	1	1.00E+10	-P	50	-T	50	-My	50	-Mz	50
-element 	flatSliderBearing	79	2	34	1	1.00E+10	-P	50	-T	50	-My	50	-Mz	50
-element 	flatSliderBearing	80	3	35	1	1.00E+10	-P	50	-T	50	-My	50	-Mz	50
-element 	flatSliderBearing	81	4	36	1	1.00E+10	-P	50	-T	50	-My	50	-Mz	50
-element 	flatSliderBearing	82	5	37	1	1.00E+10	-P	50	-T	50	-My	50	-Mz	50
-element 	flatSliderBearing	83	6	38	1	1.00E+10	-P	50	-T	50	-My	50	-Mz	50
-element 	flatSliderBearing	84	7	39	1	1.00E+10	-P	50	-T	50	-My	50	-Mz	50
-element 	flatSliderBearing	85	8	40	1	1.00E+10	-P	50	-T	50	-My	50	-Mz	50
-element 	flatSliderBearing	86	9	41	1	1.00E+10	-P	50	-T	50	-My	50	-Mz	50
-element 	flatSliderBearing	87	10	42	1	1.00E+10	-P	50	-T	50	-My	50	-Mz	50
-element 	flatSliderBearing	88	11	43	1	1.00E+10	-P	50	-T	50	-My	50	-Mz	50
-element 	flatSliderBearing	89	12	44	1	1.00E+10	-P	50	-T	50	-My	50	-Mz	50
-element 	flatSliderBearing	90	13	45	1	1.00E+10	-P	50	-T	50	-My	50	-Mz	50
-element 	flatSliderBearing	91	14	46	1	1.00E+10	-P	50	-T	50	-My	50	-Mz	50
-element 	flatSliderBearing	92	15	47	1	1.00E+10	-P	50	-T	50	-My	50	-Mz	50
-element 	flatSliderBearing	93	16	48	1	1.00E+10	-P	50	-T	50	-My	50	-Mz	50
-
-element 	flatSliderBearing	94	1	49	1	1.00E+10	-P	50	-T	50	-My	50	-Mz	50
-element 	flatSliderBearing	95	2	50	1	1.00E+10	-P	50	-T	50	-My	50	-Mz	50
-element 	flatSliderBearing	96	3	51	1	1.00E+10	-P	50	-T	50	-My	50	-Mz	50
-element 	flatSliderBearing	97	4	52	1	1.00E+10	-P	50	-T	50	-My	50	-Mz	50
-element 	flatSliderBearing	98	5	53	1	1.00E+10	-P	50	-T	50	-My	50	-Mz	50
-element 	flatSliderBearing	99	6	54	1	1.00E+10	-P	50	-T	50	-My	50	-Mz	50
-element 	flatSliderBearing	100	7	55	1	1.00E+10	-P	50	-T	50	-My	50	-Mz	50
-element 	flatSliderBearing	101	8	56	1	1.00E+10	-P	50	-T	50	-My	50	-Mz	50
-element 	flatSliderBearing	102	9	57	1	1.00E+10	-P	50	-T	50	-My	50	-Mz	50
-element 	flatSliderBearing	103	10	58	1	1.00E+10	-P	50	-T	50	-My	50	-Mz	50
-element 	flatSliderBearing	104	11	59	1	1.00E+10	-P	50	-T	50	-My	50	-Mz	50
-element 	flatSliderBearing	105	12	60	1	1.00E+10	-P	50	-T	50	-My	50	-Mz	50
-element 	flatSliderBearing	106	13	61	1	1.00E+10	-P	50	-T	50	-My	50	-Mz	50
-element 	flatSliderBearing	107	14	62	1	1.00E+10	-P	50	-T	50	-My	50	-Mz	50
-element 	flatSliderBearing	108	15	63	1	1.00E+10	-P	50	-T	50	-My	50	-Mz	50
-element 	flatSliderBearing	109	16	64	1	1.00E+10	-P	50	-T	50	-My	50	-Mz	50
-
-recorder Node -file S2newD.out -time -node  7 -dof 3 disp
-recorder Node -file S2newR1.out -time -node  1 -dof 3 reaction
-recorder Node -file S3newR21.out -time -node  16 -dof 3 reaction
+recorder Node -file AA2newD.out -time -node  7 -dof 3 disp
+recorder Node -file AA2newR1.out -time -node  1 -dof 3 reaction
+recorder Node -file AA3newR21.out -time -node  16 -dof 3 reaction
 
 #load
 pattern Plain 1 Linear { ;
